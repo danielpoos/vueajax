66598291a34500de1d0b0de5884ba25a3b2657f4
Poos Daniel <poosdani@gmail.com>
2022-01-25 17:18:35 +0100

    vue

--- src/components/Statue.vue
+++ src/components/Statue.vue
@@ -1,26 +1,50 @@
 <template>
-  <div>
-      <ul>
-          <StatueItem
-          v-bind:key="statue.person"
-          :statue="statue"
-          @statue-item-changed="Changed"
-          />
-      </ul>
-  </div>
+    <div>
+        <h1>Statue {{ $route.params.id }}</h1>
+        <table>
+        <tr>
+            <td>person</td>
+            <td>{{ statue.person }}</td>
+        </tr>
+        <tr>
+            <td>height</td>
+            <td>{{ statue.height }}</td>
+        </tr>
+        <tr>
+            <td>price</td>
+            <td>{{ statue.price }}</td>
+        </tr>
+    </table>
+    </div>
 </template>
-
 <script>
-import StatueItem from './StatueItem.vue'
 export default {
-    props: ['statues'],
-    components: {StatueItem},
-    methods: {
-        Changed(e) {
-            this.$emit('statue-item-changed', e)
+    data() {
+        return {
+        statue: {
+            id: null,
+            person: '',
+            height: '',
+            price: ''
+            },
         }
+    },
+  
+    methods:{
+        async loadStatue() {
+        const id =this.$route.params.id;
+        let Response = await fetch(`http://127.0.0.1:8000/api/statues/${id}`)
+        console.log(Response)
+        let data = await Response.json()
+        this.statue = {...data};
+        console.log(data)
+        console.log(id)
+        console.log(this.statue)
+        },
+    },
+    mounted(){
+        this.loadStatue();
     }
-
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,27 +1,35 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import Statue from './components/Statues.vue'
-import Statues from './components/Statues.vue'
-import Info from './components/Info.vue'
+import Paintings from './components/Paintings.vue';
+import Statues from './components/Statues.vue';
+import Statue from './components/Statue.vue';
+import Info from './components/Info.vue';
+
 
 Vue.use(VueRouter)
 Vue.config.productionTip = false
-const routes =[
+const routes = [
   {
-    path:'/',
-    component:Info,
-  },{
-    path:'/statues',
-    component:Statues,
-  },{
-    path:'/statue/:id',
-    component:Statue,
+    path: '/',
+    component: Paintings
+  },
+  {
+    path: '/statues',
+    component: Statues
+  },
+  {
+    path: '/statues/:id',
+    component: Statue
+  },
+  {
+    path: '/info',
+    component: Info
   }
 ];
 const router = new VueRouter({
   routes
-})
+});
 new Vue({
   render: h => h(App),
   router
